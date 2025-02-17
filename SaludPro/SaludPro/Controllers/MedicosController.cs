215add45d2a6da6b8f7a631f3a3f9e8994d126c5
Xavier Casilla <casillaxavier13@gmail.com>
2025-02-16 23:42:25 -0400

    SomeViewCreated

---
+++ SaludPro/SaludPro/Controllers/MedicosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SaludPro.Controllers
+{
+    public class MedicosController : Controller
+    {
+        public IActionResult ListMedicos()
+        {
+            return View();
+        }
+    }
+}
---
+++ SaludPro/SaludPro/Controllers/UsuariosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SaludPro.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        public IActionResult ListUsuarios()
+        {
+            return View();
+        }
+    }
+}
